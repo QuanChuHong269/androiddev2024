14adec8372863069a7d1fe0e4f41bf85803db31d
QuanChuHong269 <chuhongquan269@gmail.com>
2024-09-30 08:18:41 +0700

    commit code for practical work 6

--- app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -1,20 +1,18 @@
 package vn.edu.usth.weather;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ForecastFragment extends Fragment {
+public class WeatherFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // Inflate layout cho fragment
-        return inflater.inflate(R.layout.fragment_layout, container, false);
+        return inflater.inflate(R.layout.fragment_weather, container, false);
     }
 }
-
